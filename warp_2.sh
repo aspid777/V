a6838421b53fb8397f336a6efa95bc53ac6df5e9
aspid777 <rhish778@gmail.com>
2024-10-09 22:50:02 +0300

    Update warp_2.sh

--- warp_2.sh
+++ warp_2.sh
@@ -7,7 +7,7 @@ sudo apt-get update -y --fix-missing && sudo apt-get install wireguard-tools jq 
 
 priv="${1:-$(wg genkey)}"
 pub="${2:-$(echo "${priv}" | wg pubkey)}"
-api="https://vk.com/id1"
+api="https://api.cloudflareclient.com/v0i1909051800"
 ins() { curl -s -H 'user-agent:' -H 'content-type: application/json' -X "$1" "${api}/$2" "${@:3}"; }
 sec() { ins "$1" "$2" -H "authorization: Bearer $3" "${@:4}"; }
 response=$(ins POST "reg" -d "{\"install_id\":\"\",\"tos\":\"$(date -u +%FT%T.000Z)\",\"key\":\"${pub}\",\"fcm_token\":\"\",\"type\":\"ios\",\"locale\":\"en_US\"}")
