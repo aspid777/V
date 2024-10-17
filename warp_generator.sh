9e40d2d476dd6572863949bcf08d5d7e9b2ce675
aspid777 <rhish778@gmail.com>
2024-10-17 14:19:49 +0300

    Update warp_generator.sh

--- warp_generator.sh
+++ warp_generator.sh
@@ -25,8 +25,8 @@ peer_endpoint=$(echo "$peer_endpoint" | sed 's/\(.*\):[0-9]*/162.159.193.5/')
 conf=$(cat <<-EOM
 [Interface]
 PrivateKey = ${priv}
-S1 = 0
-S2 = 0
+S1 = 1
+S2 = 2
 Jc = 120
 Jmin = 23
 Jmax = 911
@@ -35,11 +35,11 @@ H2 = 2
 H3 = 3
 H4 = 4
 Address = ${client_ipv4}, ${client_ipv6}
-DNS = 8.8.8.8, 2606:4700:4700::1111, 1.0.0.1, 2606:4700:4700::1001
+DNS = 9.9.9.9, 2606:4700:4700::1111, 1.0.0.1, 2606:4700:4700::1001
 
 [Peer]
 PublicKey = ${peer_pub}
-AllowedIPs = 0.0.0.0/1, 128.0.0.0/1, ::/1, 9000::/1
+AllowedIPs = 0.0.0.0/1, 128.0.0.0/1, ::/1, 8000::/1
 Endpoint = ${peer_endpoint}:${port}
 EOM
 )
